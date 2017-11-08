694e1bfb4e3e8550b426756c33adfd5640eb9519
Andrew Oberhardt <Andrew@Code.org>
2017-11-07 17:30:16 -0800

    WIP

--- apps/test/unit/templates/SchoolAutocompleteDropdownTest.js
+++ apps/test/unit/templates/SchoolAutocompleteDropdownTest.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {expect} from '../../util/configuredChai';
+import sinon from 'sinon';
 import SchoolAutocompleteDropdown from '@cdo/apps/templates/SchoolAutocompleteDropdown';
 
 describe('SchoolAutocompleteDropdown', () => {
-  it('renders VirtualizedSelect', () => {
-    const wrapper = shallow(
+  let schoolAutocompleteDropdown;
+  let handleChange;
+  beforeEach(() => {
+    handleChange = sinon.spy();
+    schoolAutocompleteDropdown = shallow(
       <SchoolAutocompleteDropdown
-        showErrorMsg={false}
+        onChange={handleChange}
       />
     );
-    expect(wrapper.find('VirtualizedSelect').exists()).to.be.true;
   });
+
+  it('renders VirtualizedSelect', () => {
+    expect(schoolAutocompleteDropdown.find('VirtualizedSelect').exists()).to.be.true;
+  });
+
+  describe("getOptions()", () => {
+    let getOptions;
+    beforeEach(() => {
+      getOptions = schoolAutocompleteDropdown.instance().getOptions;
+    });
+
+    it("Resolves to undefined for queries less than 4 characters", () => {
+      expect(getOptions("abc")).to.eventually.equal(undefined);
+    });
+
+    it("Fetches schools from schoolsearch API for queries >= 4 characters", () => {
+      // const server = sinon.fakeServer.create();
+      getOptions("abcd");
+    });
+  });
+
 });
